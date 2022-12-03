1e50d4307178317f277ec1eec4b474cadf104534
Eduard <ptushko@gmail.com>
2022-12-03 21:43:11 +0300

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Eduard on 03.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
